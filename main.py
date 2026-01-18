7049c52e2292da48f335949fa07056a695f50c79
Barrett <barrettbecker19@gmail.com>
2026-01-18 11:12:59 -0500

    made a app :)

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
